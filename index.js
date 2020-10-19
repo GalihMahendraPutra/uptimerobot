3308ccd7a961b374e73fa432d63fa5745d7a91b3
GalihMahendraPutra <73109949+GalihMahendraPutra@users.noreply.github.com>
2020-10-19 18:31:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://botjarvis001.glitch.me.https://botjarvis02.glitch.me"]
+const urls = ["https://botjarvis02.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
